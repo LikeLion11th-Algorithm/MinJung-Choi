d7c05f84524902f2d60b36b9fd6e2213449fb199
Choi MinJung <mj020503@naver.com>
2023-04-16 19:35:59 +0900

    230416 N번째 큰 수(배열)-메모리초과

--- src/BOJ_2075.java
+++ src/BOJ_2075.java
@@ -4,42 +4,42 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 public class BOJ_2075 {
 	
-	static int sort(int[][] a, int key) {
-		int []b = new int[key * key ];
+	static int sort(int[][] arr, int N) {
+		int[] newArr = new int[N * N];
 		int temp = 0;
 		int k = 0;
 		//2차원 배열을 1차원 배열로 전환
-		for(int i = 0; i <= a.length-1; i++) 
-			for(int j = 0; j <= a[i].length-1; j++) {
-				b[k] = a[i][j]; 
+		for(int i = 0; i <= arr.length-1; i++)
+			for(int j = 0; j <= arr[i].length-1; j++) {
+				newArr[k] = arr[i][j];
 				k++;
 			}
 		//선택정렬_내림차순
 		for(int i = 0; i < k-2; i++) {
 			for(int j = i+1; j < k; j++) {
-				if(b[i] < b[j]) {
-					temp = b[i];
-					b[i] = b[j];
-					b[j] = temp;
+				if(newArr[i] < newArr[j]) {
+					temp = newArr[i];
+					newArr[i] = newArr[j];
+					newArr[j] = temp;
 				} 
 			}
 		}
-		return b[key-1]; 
+		return newArr[N-1];
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[][] Num = new int[N][N];
+		int[][] arr = new int[N][N];
 
 		for(int i = 0; i <= N-1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			for(int j = 0; j <= N-1; j++) {
-				Num[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int ord = sort(Num, N);
+		int ord = sort(arr, N);
 		System.out.println(ord);	
 	}
 }
