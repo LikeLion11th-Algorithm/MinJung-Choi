3775a0e5cd70e556f0855170dad9ba8bc1f865a0
Choi MinJung <mj020503@naver.com>
2023-04-29 00:01:59 +0900

    230429 단어 길이 재기

---
+++ src/BOJ_2743.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class BOJ_2743 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+    }
+}
